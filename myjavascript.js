585d70fcd4032c84d7042d16a479d9dc4241a16e
svasanthas <36577330+svasanthas@users.noreply.github.com>
2020-06-07 19:26:06 +0530

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -3,5 +3,7 @@ let fullname="vasantha setty"
 let nid="hng 07"
 let lang="javascript"
 let email="svasanthas@yahoo.com"
-console.log("Hello world "+fullname + " hng id "+
-}+ hid + " lang is "+ lang + " email "+ email
+
+console.log("Hello world, this is "+fullname + " with HNHi7 ID "+
++ hid + " using language "+ lang + " for stack2 task and my email id is "+ email);
+}
