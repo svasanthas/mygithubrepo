2e06086684bd0b71ee4fc1b90704ce492f2efa41
svasanthas <36577330+svasanthas@users.noreply.github.com>
2020-06-07 19:17:52 +0530

    Create myjavascript.js

---
+++ myjavascript.js
@@ -0,0 +1,7 @@
+function showresult(){
+let fullname="vasantha setty"
+let nid="hng 07"
+let lang="javascript"
+let email="svasanthas@yahoo.com"
+console.log("Hello world "+fullname + " hng id "+
+}+ hid + " lang is "+ lang + " email "+ email
